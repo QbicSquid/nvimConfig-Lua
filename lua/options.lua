e3eb84ba2398491a37d3b50319a7e5d38b614d57
QbicSquid <shifan611@gmail.com>
2022-08-08 18:59:44 +0530

    Initial commit with basic options

---
+++ lua/options.lua
@@ -0,0 +1,35 @@
+-- Cursor
+vim.o.cursorline = true
+
+-- Folds
+vim.o.foldmethod = 'indent'
+vim.o.foldlevel = 99
+
+-- General
+vim.o.fillchars = 'eob: '
+vim.o.wrap = false
+vim.o.updatetime = 750
+
+-- Numbering
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- Search and Highlighting
+vim.o.smartcase = true
+vim.o.hlsearch = true
+
+-- Splits
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- Statusline
+vim.o.laststatus = 3
+vim.o.showmode = false
+
+-- Tab Style
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.mouse = 'a'
+
---
+++ plugin/highlights.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_hl(0, 'CursorLine', { bg='NONE' })
+vim.api.nvim_set_hl(0, 'CursorLineNR', { bg='NONE', fg='ORANGE' })
