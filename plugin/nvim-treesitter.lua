8dfe37537eea30646bea68de443a3ff955a92701
QbicSquid <shifan611@gmail.com>
2022-09-04 13:18:02 +0530

    Added Treesitter indentation

--- plugin/nvim-treesitter.lua
+++ plugin/nvim-treesitter.lua
@@ -27,6 +27,13 @@ require'nvim-treesitter.configs'.setup {
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
     },
+
+    -- experimental feature
+    indent = {
+        enable = true,
+        disable = { 'python', 'c' },
+    },
+
     incremental_selection = {
         enable = true,
         keymaps = {
